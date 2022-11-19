e5c9d4b2aeba5544d0ba6fb308a0d74a4d147563
MEOW_MEOW_ME <80235976+joemar25@users.noreply.github.com>
2022-11-19 01:59:40 +0000

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# SE1_Project
+# SE1 Project
+
 This Repository is for our Group project in our Software Engineering 1
+
+Folders
+
+- Unintegrated
+  - a folder contains pure python programs which is not yet integrated on the web
+- Integrated
+  - a folder contains the (unintegrated python final codes) that is implemented on the web
---
+++ Unintegrated/basics/basic_01.py
@@ -0,0 +1,35 @@
+'''
+@todo
+    - audio file formats
+    - audio processing basics
+    - signal parameters
+    - wave module
+    - plot waveform
+    - microphone recording
+    - load other audio formats
+
+
+    @audio file formats
+        example: .mp3, .flac, .wav, .ogg, etc.
+
+        dictionary: 'compress' is a process
+
+        mp3 - most popular, but a lossy compression format
+              means that it compresses
+              the data that it can lose information
+        flac - also popular, but a loss lesscompression format
+              means that it compresses
+              the data but allows us to perfectly reconstruct
+              the original data
+        wav - (stands for: wave form audio format)
+              is a uncompressed format
+              means that it stores data in a uncompressed way.
+              the audio quality here is the best but the
+              file size is the largest
+
+        note: we will use wav since it is easier to work with in python
+
+              importing wave requires no installation (it is pre-build)
+'''
+
+# import wave
