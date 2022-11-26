3ddff6d09dfae2f10b48d208290f0bd09b96a2bc
joemar25 <joemar.cardino000@gmail.com>
2022-11-26 09:32:25 +0800

    Update

--- testing_area(25-11-22)/Generator.py
+++ testing_area(25-11-22)/Generator.py
@@ -1,17 +1,22 @@
+import uuid
+import pytz
+from datetime import datetime
+
+
 class Score:
 
     '''
         this is just a test...
     '''
 
-    def grammar(self, text_data: list[str], correction: list[str]): 
+    def grammar(self, input_text: list[str], correct_text: list[str]):
 
         # variables
         word_count: int = 0
         mistake: int = 0
         score: float = 0
 
-        for sentence_a, sentence_b in zip(text_data, correction):
+        for sentence_a, sentence_b in zip(input_text, correct_text):
 
             # split the sentences, so we can get no 'spaces'
             sentence_a = sentence_a.split(' ')
@@ -29,3 +34,20 @@ class Score:
         # score = 100% - (mistake over total count times 100)
         score = 100-(mistake/word_count*100)
         return score
+
+
+# File_Name_Generator Class
+class File_Name:
+
+    def file_name(self):
+        # uses: uuid, pytz, datetime
+        todays: datetime = datetime.now(pytz.timezone('Asia/Manila')).utcnow()
+        utime: str = f'{uuid.uuid1()}{todays.hour}{todays.minute}{todays.second}'
+
+        deduct :int= -2 if str(todays.year)[1] == '0' else -3
+        udate :str= f"{str(todays.year)[deduct:]}{todays.month}{todays.day}"
+
+        return f"{udate}{utime}"
+
+    def wav_file_name(self):
+        return f'{self.file_name()}.wav'
--- testing_area(25-11-22)/__main__.py
+++ testing_area(25-11-22)/__main__.py
@@ -1,31 +1,40 @@
-from Score_Generator import Score
+from Generator import Score, File_Name
 
 
 def main():
 
     # instances
     check = Score()
+    name = File_Name()
 
     # suppose, it is already in (clean state)
     # suppose, a raw data example
-    text :list[str]= [
+    text: list[str] = [
         'my name is Joemar',
         'and i lives in my houses',  # suppose 'live' and 'house'
         'i do a lot of chores',
     ]
 
     # do grammar check, below is just an example
-    correct :list[str]= [
+    correct: list[str] = [
         'my name is Joemar',
         'and i live in my house',  # suppose 'live' and 'house'
         'i do a lot of chores',
     ]
 
-    # get score(s)
-    grammar_score : float= check.grammar(text, correct)
+    # ################################################################### #
 
-    # print score(s)
-    print(f'grammar score: {grammar_score}')
+    # get file name for the wav
+    name = name.wav_file_name()
+
+    print(f'file name: {name}')
+
+    # ################################################################### #
+
+    # get score(s)
+    grammar_score: float = check.grammar(text, correct)
+
+    print(f'grammar score: {grammar_score}%')
 
 
 if __name__ == "__main__":
