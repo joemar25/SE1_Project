5ae7b53ed4c1feb8a9f12cb550b0d91ca3f0fb0b
joemar25 <joemar.cardino000@gmail.com>
2022-11-26 19:52:49 +0800

    Update

--- testing_area(25-11-22)/Generator.py
+++ testing_area(25-11-22)/Generator.py
@@ -1,14 +1,9 @@
 ''''
     this generator file contains all the generator classes for returning [Score(s), File Name, ...]
     
-    - Scores contain, how user's grammar is accurately correct
-                      .......... rate (speed)
-                      .......... pitch
-                      .......... articulation
-                      .......... prounounciation
-                      .......... volume
+    - Scores contain : rate (speed), pitch, articulation, prounounciation, volume
     
-    - File Name, contains the generated filename. Used for saving a file.
+    - File Name : contains the generated filename. Used for saving a file.
 '''
 
 import uuid
@@ -18,11 +13,11 @@ from datetime import datetime
 
 class Score:
 
-    '''
-        this is just a test...
-    '''
-
     def grammar(self, input_text: list[str], correct_text: list[str]) -> float:
+        '''
+        Grammar
+            Using Grammarly API
+        '''
 
         # variables
         word_count: int = 0
@@ -49,20 +44,50 @@ class Score:
         return score
 
     # speed
-    def rate(self, input_text: list[str], correct_text: list[str]):
-        ...
+    def rate(self):
+        '''
+        Rate 
+            Words / Time 
+            140  to 160 wpm [ideal]
+            2.33 to 2.67 score 
 
-    def pitch(self, input_text: list[str], correct_text: list[str]):
-        ...
+            Note: Lower is Slower
+                Higher is Faster
+        '''
 
-    def articulation(self, input_text: list[str], correct_text: list[str]):
-        ...
+    def pitch(self):
+        '''
+        Pitch
+            Male [85-180 hertz]
+            Female [165-255 hertz]
 
-    def prounounciation(self, input_text: list[str], correct_text: list[str]):
-        ...
+            Note: this can be seen in Spectrogram's 'Y-Axis'
+        '''
 
-    def volume(self, input_text: list[str], correct_text: list[str]):
-        ...
+    def articulation(self):
+        '''
+        Articulation
+            Using MAR (Mean Articulatory Rate)
+            10 - 20 consecutive syllables 
+
+            Note: this is for fluent speech without pauses, MAR technique is a reliable approach for determining articulation rate.
+        '''
+
+    def prounounciation(self):
+        '''
+        Pronunciation
+            Using Google Dictionary API
+        '''
+
+    def volume(self):
+        '''
+        Volume
+            Represented by Colours
+            Low Volume  ⇒ Black to Red
+            High Volume ⇒ Yellow to White
+
+            Note: our group is still thinking if we are going to specify a specific speech distance when using the app
+        '''
 
 
 # File_Name_Generator Class
@@ -99,19 +124,21 @@ class Feedback:
             'volume': volume_score,
         }
 
-    def __get_total_score(self) -> float:
+    def __get_total_avg_score(self) -> float:
         generated_total = 0
-        for i in self.scores:
-            generated_total += self.scores[i]
+        for index in self.scores:
+            generated_total += self.scores[index]
         return generated_total / self.TOTAL
 
     def feedback(self):
         score = self.scores
-        total_score = self.__get_total_score()
+        total_score = self.__get_total_avg_score()
 
-        for i in self.scores:
-            print(f'{i}: ', end='')
-            print('good') if score[i] > 85 else print('bad')
+        for index in self.scores:
+            print(f'{score[index]}%\tfor {index} -> \tremarks: ', end='')
+            print('good') if score[index] > 85 else print('bad')
 
-        print('overall score feedback: ', end='')
+        print('\noverall score feedback: ', end='')
         print('good') if total_score > 85 else print('bad')
+
+        # generate the words for feedback
--- testing_area(25-11-22)/testfeedback.py
+++ testing_area(25-11-22)/testfeedback.py
@@ -1,5 +1,6 @@
 from Generator import Feedback
+import os
 
-
+os.system('cls')
 feed = Feedback(86, 86, 86, 86, 86, 100)
 feed.feedback()
